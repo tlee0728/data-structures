2c5e5f661d5fc256a945d7b0e600e092d960cbf9
Timothy Lee <tlee728@berkeley.edu>
2019-05-29 14:18:08 -0700

    implemented stack class using es6 method

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,31 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // push, pop, size
   constructor() {
+  	this.storage = {};
+  	this.sizeStack = 0;
+  }
+
+  push(value) {
+  	this.storage[this.sizeStack] = value;
+  	this.sizeStack += 1;
+  }
+
+  pop() {
+  	if (this.sizeStack === 0) {
+  		return;
+  	}
+
+  	var toReturn = this.storage[this.sizeStack - 1];
+  	delete this.storage[this.sizeStack - 1];
+  	this.sizeStack -= 1;
+
+  	return toReturn;
+  }
+
+  size() {
+  	return this.sizeStack;
   }
 
 }
