10d841e5fa7558060bd713646170c7a1ed7927ad
Timothy Lee <tlee728@berkeley.edu>
2019-05-29 15:03:09 -0700

    pomander on es6

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,30 +2,31 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   // push, pop, size
+
   constructor() {
-  	this.storage = {};
-  	this.sizeStack = 0;
+    this.storage = {};
+    this.sizeStack = 0;
   }
 
   push(value) {
-  	this.storage[this.sizeStack] = value;
-  	this.sizeStack += 1;
+    this.storage[this.sizeStack] = value;
+    this.sizeStack += 1;
   }
 
   pop() {
-  	if (this.sizeStack === 0) {
-  		return;
-  	}
+    if (this.sizeStack === 0) {
+      return;
+    }
 
-  	var toReturn = this.storage[this.sizeStack - 1];
-  	delete this.storage[this.sizeStack - 1];
-  	this.sizeStack -= 1;
+    var toReturn = this.storage[this.sizeStack - 1];
+    delete this.storage[this.sizeStack - 1];
+    this.sizeStack -= 1;
 
-  	return toReturn;
+    return toReturn;
   }
 
   size() {
-  	return this.sizeStack;
+    return this.sizeStack;
   }
 
 }
