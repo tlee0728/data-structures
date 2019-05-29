2932fb9c925c152a172e4bb6e1ce3ea6b53199ff
Timothy Lee <tlee728@berkeley.edu>
2019-05-29 11:50:34 -0700

    implemented queue data structure. passed all test cases.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,39 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size += 1;
   };
 
+  /*
+  0 --> 'a'
+  1 --> 'b'
+  2 --> 'c'
+  3 --> 'd'
+  */
+
   someInstance.dequeue = function() {
+    if (size === 0) {
+      return;
+    }
+    var toReturn = storage[0];
+    delete storage[0];
+    size -= 1;
+    // shift all the keys down by 1 using for loop
+    for (var i = 0; i < size; i++) {
+      storage[i] = storage[i+1];
+    }
+
+    return toReturn;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
