cac795f7b307c8175cec8f66a73871772994ed88
Timothy Lee <tlee728@berkeley.edu>
2019-05-29 13:00:09 -0700

    changed stack to match format of functional-shared data structure

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,31 +2,35 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   // push, pop, size
-  var storage = {};
-  var size = 0;
-  stackMethods.push = function(value) {
-  	storage[size] = value;
-  	size += 1;
-  };
+  var newStack = {};
+  newStack.storage = {};
+  newStack.sizeQueue = 0;
 
-  stackMethods.pop = function() {
-  	if (size === 0) {
-  		return;
-  	}
-  	var toReturn = storage[size - 1];
-  	delete storage[size - 1];
-  	size -= 1;
+  _.extend(newStack, stackMethods);
 
-  	return toReturn;
-  };
-
-  stackMethods.size = function() {
-  	return size;
-  };
-
-  return stackMethods;
+  return newStack;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.sizeQueue] = value;
+  this.sizeQueue += 1;
+};
+
+stackMethods.pop = function() {
+  if (this.sizeQueue === 0) {
+    return;
+  }
+  var toReturn = this.storage[this.sizeQueue - 1];
+  delete this.storage[this.sizeQueue - 1];
+  this.sizeQueue -= 1;
+
+  return toReturn;
+};
+
+stackMethods.size = function() {
+  return this.sizeQueue;
+};
 
 
