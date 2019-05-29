e95af579ad938ad74623094e00720acf90da3e0f
Timothy Lee <tlee728@berkeley.edu>
2019-05-29 11:49:39 -0700

    finished implementing test cases for stack. passed all test cases.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,27 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  // create variable that stores the key/index of the next value.
+  var nextIndex = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[nextIndex] = value;
+    nextIndex += 1;
   };
 
   someInstance.pop = function() {
+    if (nextIndex === 0) {
+      return;
+    }
+    var toReturn = storage[nextIndex - 1];
+    delete storage[nextIndex - 1];
+    nextIndex -= 1;
+    return toReturn;
   };
 
   someInstance.size = function() {
+    return nextIndex;
   };
 
   return someInstance;
