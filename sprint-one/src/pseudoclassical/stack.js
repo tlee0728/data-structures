c3cb118d8c2b3253ff9144e48997b1a23f9975d9
Timothy Lee <tlee728@berkeley.edu>
2019-05-29 14:08:46 -0700

    implemented stack using the psuedoclassical method

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // push, pop, size
+  this.sizeStack = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+	this.storage[this.sizeStack] = value;
+	this.sizeStack += 1;
+}
+
+Stack.prototype.pop = function() {
+	if (this.sizeStack === 0) {
+		return;
+	}
+
+	var toReturn = this.storage[this.sizeStack - 1];
+	delete this.storage[this.sizeStack - 1];
+	this.sizeStack -= 1;
+
+	return toReturn;
+}
+
+Stack.prototype.size = function() {
+	return this.sizeStack;
+}
 
