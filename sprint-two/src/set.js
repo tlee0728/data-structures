691c0cc5bfc766b4c1a521ee17d99443fc7a1d79
Timothy Lee <tlee728@berkeley.edu>
2019-05-30 15:19:25 -0700

    implemented set data structure

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,47 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // object or array
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  // iterate through each element in the storage
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  // check variable to indicate if item has been found to remove
+  // after finding the variable, shift the index of every value after down by 1.
+  var check = false;
+  for (var i = 0; i < this._storage.length; i++) {
+    if (i === this._storage.length - 1) {
+      this._storage.pop();
+    } else if (check) {
+      this._storage[i] = this._storage[i + 1];
+    }
+    
+    // if item is found, change check variable to true for future iteration to shift future indices.
+    if (this._storage[i] === item) {
+      check = true;
+      this._storage[i] = this._storage[i + 1];
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ add: O(1)
+ contains: O(n)
+ remove O(n)
  */
