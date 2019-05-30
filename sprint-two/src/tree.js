3fd7c93360e37b9df7ba8f2e07316534aa0f2b55
Timothy Lee <tlee728@berkeley.edu>
2019-05-30 12:17:22 -0700

    implemented tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,45 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; 
+  _.extend(newTree,treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  // use helper function to recursively go through all the child nodes.
+  // create check variable to see if value has been found. change check to true if found.
+  var check = false;
+
+  var helper = function(currentTree) {
+    if (check) {
+      return;
+    }
+    if (currentTree.value === target) {
+      check = true;
+      return;
+    }
+
+    // check all children trees.
+    for (var i = 0; i < currentTree.children.length; i++) {
+      helper(currentTree.children[i]);
+    }
+
+  };
+  helper(this);
+  return check;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ for addChild: constant time
+ for contains: long n time
  */
